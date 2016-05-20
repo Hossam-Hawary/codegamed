bfd0dbb6a35219bf33152ee3fddbbed3351d612b
Hossam Hawary <Hossam0hawary@gmail.com>
2016-05-20 16:07:36 +0200

    Create New Level As Admin-new action with empty form

--- app/controllers/levels_controller.rb
+++ app/controllers/levels_controller.rb
@@ -1,8 +1,12 @@
 class LevelsController < ApplicationController
-before_filter :login
+# before_filter :login
+# before_action :set_category, only: [:show, :edit, :update, :destroy]
 	def login
 		if current_user == nil
 			redirect_to root_path
 		end
+	end
+	def new
+		@level=Level.new
 	end
 end
