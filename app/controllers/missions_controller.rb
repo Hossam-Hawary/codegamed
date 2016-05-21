a75956af9f4cf3f3c0ccc49acbe1b1fd00ecdbe2
Hossam Hawary <Hossam0hawary@gmail.com>
2016-05-21 16:02:24 +0200

    Create New Mission As Admin-handle maximum order

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -9,15 +9,28 @@ class MissionsController < ApplicationController
 
   def create
     @mission=Mission.new(mission_params)
-
-
     Level.find_by id: @mission.level.missions.each do |mission|
       if mission.order>= @mission.order
         Mission.update(mission,"order"=>mission.order+1)
       end
     end
+    maxorder=((Level.find_by id: @mission.level).missions.maximum("order"))
+    if maxorder
+      if @mission.order>(maxorder+1)
+        @mission.order=maxorder+1
+      end
+    else
+      @mission.order=1
+    end
 
 
+    respond_to do |format|
+      if @mission.save
+        format.html { redirect_to missions_path, notice: 'Mission was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
   end
 
   private
