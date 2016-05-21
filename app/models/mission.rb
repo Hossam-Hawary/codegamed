649bfac48e1cb6262a50234d51052fd2dd8c6556
Hossam Hawary <Hossam0hawary@gmail.com>
2016-05-21 18:03:45 +0200

    Create New Test Cases as Admin

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -1,5 +1,6 @@
 class Mission < ActiveRecord::Base
   belongs_to :level
+  has_many :test_cases
   validates :level_id, :problem,:score,:presence => true
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ root 'main_page#index'
 post '/levels/show_user_levels' => 'levels#show_user_levels'
 resources :badges
 resources :missions
+resources :test_cases
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
