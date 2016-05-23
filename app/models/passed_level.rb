d7faa94ebc9a192310f29418d0ef50fc9356d2bb
Ramez <rmz.aziz@gmail.com>
2016-05-23 11:56:11 +0200

    open level and mission for new users

--- app/models/passed_level.rb
+++ app/models/passed_level.rb
@@ -1,4 +1,14 @@
 class PassedLevel < ActiveRecord::Base
   belongs_to :level
   belongs_to :user
+
+  def self.set_level(user_id)
+    where(user_id: user_id).first_or_initialize do |passed_level|
+      passed_level.user_id = user_id
+      passed_level.level_id=1
+      passed_level.last_mission_order=1
+      passed_level.save!
+    end
+  end
+
 end
