05f35f810c51890d2fed50ae1d8e22af76622b5e
Hossam Hawary <Hossam0hawary@gmail.com>
2016-05-31 17:45:45 +0200

    open_first_mission-for new users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
     #@facebook = Koala::Facebook::API.new(user.oauth_token)
     #@facebook.get_object("me?fields=friends,taggable_friends")
     PassedLevel.set_level(user.id)
+    PassedMission.open_first_mission(user.id)
     redirect_to root_url
   end
 
--- app/models/passed_mission.rb
+++ app/models/passed_mission.rb
@@ -1,4 +1,16 @@
 class PassedMission < ActiveRecord::Base
   belongs_to :user
   belongs_to :mission
+  def self.open_first_mission(user)
+    where(user_id: user).first_or_initialize do |mission_one|
+
+      mission_one=self.new
+      mission_one.user_id=user
+      last_level_id=Level.maximum("order")
+      mission_id=Mission.find_by order:1,level_id:last_level_id
+      mission_one.mission_id=mission_id.id
+      mission_one.save
+    end
+
+  end
 end
