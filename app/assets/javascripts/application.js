1cd4105a57143742c45ef0ece6981891661953ba
4m3ndy <ahmed2hamdy@gmail.com>
2016-06-01 23:08:14 +0200

    Enabling jQuery UI through jQuery UI GEM

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require angular.js
+//= require jquery-ui
 //= require ckeditor-jquery
 //= require angular-route.js
 //= require angular-animate.js
