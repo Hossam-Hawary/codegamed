199a0fcc38f636ac08593b70c51d0b0d46980a48
Hossam Hawary <Hossam0hawary@gmail.com>
2016-06-03 18:20:00 +0200

    open new mission or level when success

--- app/models/passed_mission.rb
+++ app/models/passed_mission.rb
@@ -2,16 +2,16 @@ class PassedMission < ActiveRecord::Base
   belongs_to :user
   belongs_to :mission
 
-  def self.open_new_mission(user_id,mission_order)
-
-    @mission_one = self.new
-    @mission_one.user_id = user_id
-    last_passed_level = (User.find_by id:user_id).levels.maximum("order")
-    mission_id = Mission.find_by order:mission_order,level_id:last_passed_level
-    @mission_one.mission_id = mission_id.id
-    @mission_one.save
+  def self.open_new_mission(user_id, mission_order, level_id)
+    mission = Mission.find_by order: mission_order, level_id: level_id
+    record_exist=self.find_by mission_id: mission.id, user_id: user_id
+    if !record_exist
+      @mission_one = self.new
+      @mission_one.user_id = user_id
+      @mission_one.mission_id = mission.id
+      @mission_one.save
+    end
 
   end
-
 
 end
