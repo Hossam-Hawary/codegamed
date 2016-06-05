167917882d655e3d5a4e0cf667f1d42f5fe816fe
Hossam Hawary <Hossam0hawary@gmail.com>
2016-06-05 14:23:21 +0200

    fix new user bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   has_many :levels, through: :passed_levels
   has_many :passed_missions
   has_many :missions, through: :passed_missions
-  after_save :open_first_level_and_mission
+  after_create :open_first_level_and_mission
 
 # Check user existance in db and save new users 
   def self.from_omniauth(auth)
@@ -17,14 +17,21 @@ class User < ActiveRecord::Base
       user.email=auth.info.email
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.total_score=0
       user.save!
 
     end
   end
+  def rais_score(mission)
+    last_passed_mission=self.missions.last
+    if last_passed_mission==mission
+    User.update(self, "total_score" => self.total_score + mission.score)
+    end
+  end
+
   private
   def open_first_level_and_mission
-    user_id=User.maximum("id")
-    PassedLevel.open_new_level(user_id,1)
-    PassedMission.open_new_mission(user_id,1,(PassedLevel.last_level current_user).id)
+    PassedLevel.open_new_level(self.id,1)
+    PassedMission.open_new_mission(self.id,1,(PassedLevel.last_level self).id)
   end
 end
