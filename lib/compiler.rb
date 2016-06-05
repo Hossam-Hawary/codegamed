4b4b86b8d8d8897d366f237d2e2645101791e245
4m3ndy <ahmed2hamdy@gmail.com>
2016-06-05 14:23:52 +0200

    Adding the Java.rb Class that extends Compiler Class

--- lib/compiler.rb
+++ lib/compiler.rb
@@ -1,40 +1,7 @@
 class Compiler
 
-  def self.compile_user_code (submitted_code)
-    test_cases.each do |tc|
-
-      java_code = 'public class Code{
-
-          ' + submitted_code + '
-
-          public static void main(String args[]){
-
-            Code c = new Code();
-            int x = c.' + tc.input + ';
-            System.out.println(""+x);
-
-          }
-        }'
-
-      my_file = File.new("Code.java", "w+")
-      my_file.puts(java_code)
-      my_file.close
-      File.chmod(0777,"Code.java")
-      stdin, stdout, stderr = Open3.popen3('javac Code.java')
-
-      puts stderr.gets
-      $result = `timeout 4s java Code`
-
-      if $result.chomp != tc.output
-        render :json =>  {'output':'Failure'}
-        return
-      end
-
-      File.delete("Code.java")
-      File.delete("Code.class")
-
-    end
-
+  def compile_user_code (user_submitted_code,mission_Obj)
+    #compile_user_code should be implemented in the subclass
   end
 
 end
