39f00ca2c31ca61d51e91f6fb4d1191ba93aee72
Hossam Hawary <Hossam0hawary@gmail.com>
2016-06-06 13:04:28 +0200

    add progress bar to passed_levels

--- app/models/passed_level.rb
+++ app/models/passed_level.rb
@@ -9,10 +9,17 @@ class PassedLevel < ActiveRecord::Base
         passed_level.user_id = user_id
         passed_level.level_id=level.id
         passed_level.last_mission_order=1
+        passed_level.progress=0
         passed_level.save!
         level=passed_level.level
     end
     level
+  end
+  def self.raise_progress(user,mission)
+    passed_level=PassedLevel.find_by user_id:user.id, level_id:mission.level.id
+    progress=(mission.order.to_f/mission.level.missions.size)*100
+    passed_level.progress=progress
+    passed_level.save!
   end
 
   def self.last_level(user)
