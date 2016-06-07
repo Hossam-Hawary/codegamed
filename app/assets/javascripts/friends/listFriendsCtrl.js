8aa148022f3ec4c41a06fa16eb94e56cca434090
Te7a-Houdini <ahmedabdelftah95165@gmail.com>
2016-06-07 02:38:42 +0200

    Search For Friends

--- app/assets/javascripts/friends/listFriendsCtrl.js
+++ app/assets/javascripts/friends/listFriendsCtrl.js
@@ -1,7 +1,7 @@
 /**
  * Created by te7a on 05/06/16.
  */
-angular.module('codeGamed').controller('listFriendsCtrl',function($scope,$mdSidenav,listUserFriendsFactory){
+angular.module('codeGamed').controller('listFriendsCtrl',function($scope,$mdSidenav,listUserFriendsFactory,$q){
 
     listUserFriendsFactory.listFriends().then(function(res){
         $scope.friends = res.friends;
@@ -23,5 +23,41 @@ angular.module('codeGamed').controller('listFriendsCtrl',function($scope,$mdSide
     };
 
 
+
+    // search for a new friends
+
+    
+    $scope.selectedItemChange=function(item){
+
+        if (typeof(item) != "undefined")
+        {
+
+            $scope.addFriend=item;
+        }
+
+        else{
+
+            $scope.addFriend=false;
+        }
+
+    }
+
+    $scope.searchTextChange=function(searchText){
+
+        if (searchText.trim() == '')
+            return [];
+
+
+        var def = $q.defer();
+
+        listUserFriendsFactory.searchNewFriends(searchText).then(function(res){
+
+            def.resolve(res.new_friends);
+        });
+
+       
+     return def.promise;
+
+    }
     
 });
