9b71c858215cce8719a7c8177d883dc839f3c5c9
4m3ndy <ahmed2hamdy@gmail.com>
2016-06-08 23:20:57 +0200

    Adding the route of the get_user_data action in routes.rb

--- app/controllers/main_page_controller.rb
+++ app/controllers/main_page_controller.rb
@@ -1,26 +1,31 @@
 class MainPageController < ApplicationController
-def list_user_friends
-  @friends=[]
-  
+  def list_user_friends
+    @friends=[]
 
-  current_user.friends.each do |friend|
-  	@friend={}
-    @friend[:id] = friend.id
-    @friend[:name] = friend.name
-    @friend[:image] = friend.image_url
-    @friend[:score] = friend.total_score
-    @friends.push(@friend)
+
+    current_user.friends.each do |friend|
+      @friend={}
+      @friend[:id] = friend.id
+      @friend[:name] = friend.name
+      @friend[:image] = friend.image_url
+      @friend[:score] = friend.total_score
+      @friends.push(@friend)
+    end
+
+    render :json => {'friends':@friends , 'requests':current_user.requested_friendships.select("name,image_url,user_id,total_score")}
+
   end
 
-  render :json => {'friends':@friends , 'requests':current_user.requested_friendships.select("name,image_url,user_id,total_score")}
+  def search_for_new_friends
 
-end
+    @search_word = params[:search_word]
+    @current_user_new_friends = User.where("name LIKE '%#{@search_word}%'").select("name,image_url,id").limit(10) - current_user.all_friendships
+    @current_user_new_friends.delete(current_user)
+    render :json => {'new_friends':@current_user_new_friends}
+  end
 
-def search_for_new_friends
+  def get_user_data
 
-  @search_word = params[:search_word]
-  @current_user_new_friends = User.where("name LIKE '%#{@search_word}%'").select("name,image_url,id").limit(10) - current_user.all_friendships
-  @current_user_new_friends.delete(current_user)
-  render :json => {'new_friends':@current_user_new_friends}
-end
+  end
+
 end
