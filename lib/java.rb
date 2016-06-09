b9e02ee5dd58fae9c79f84174c71491a2c821def
4m3ndy <ahmed2hamdy@gmail.com>
2016-06-09 14:52:42 +0200

    Execute Code in case Code.class exists

--- lib/java.rb
+++ lib/java.rb
@@ -42,7 +42,7 @@ class Java < Code
       stdin, stdout, stderr = Open3.popen3('javac Code.java')
 
       puts stderr.gets
-      $result = `timeout 4s java Code`
+      $result = `timeout 4s java Code` if File.exist?("Code.class")
 
       if $result.chomp != tc.output
         File.delete("Code.java") if File.exist?("Code.java")
