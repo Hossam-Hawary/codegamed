6b5677791e7c3c8aa59b1e11abb4f30f33f9dbe7
Ubuntu <ubuntu@ip-172-31-34-34.us-west-2.compute.internal>
2016-06-11 22:16:03 +0000

    set server errors in environment

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-config.consider_all_requests_local = true
-ActiveRecord::RecordNotFound => 404 (page not found)
-nil.method => 500 (server error) unless you turn off whiny nils
-ActionController::RoutingError => 404 (page not found)
--- config/routes.rb
+++ config/routes.rb
@@ -37,6 +37,7 @@ Rails.application.routes.draw do
   root 'main_page#index'
 
   resources :friendships
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
