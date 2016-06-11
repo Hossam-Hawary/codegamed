545dee040e7e9d14e2f74ac6ca932b9f7bb9310f
4m3ndy <ahmed2hamdy@gmail.com>
2016-06-12 00:34:03 +0200

    Optimzing java.rb By Adding a directory user and add the return type in the java code

--- lib/java.rb
+++ lib/java.rb
@@ -5,22 +5,20 @@ class Java < Code
   def compile_user_code(user_submitted_code, mission_Obj, user_id)
 
     test_cases = TestCase.where(mission_id: mission_Obj.id)
+    dir_path = Rails.root.join('tmp').to_s + '/' + user_id
 
-    if Dir.exist? Rails.root.join('tmp') + user_id
-      Dir.open Rails.root.join('tmp').to_s + '/' + user_id do
-        return check_with_test_cases(test_cases, user_submitted_code)
-      end
+    if Dir.exist? dir_path
+      return check_with_test_cases(test_cases, user_submitted_code, dir_path)
     else
-      Dir.mkdir Rails.root.join('tmp') + user_id do
-        return check_with_test_cases(test_cases, user_submitted_code)
-      end
+      Dir.mkdir dir_path
+      return check_with_test_cases(test_cases, user_submitted_code, dir_path)
     end
 
   end
 
 
 
-  def check_with_test_cases(test_cases,user_submitted_code)
+  def check_with_test_cases(test_cases,user_submitted_code, dir_path)
 
     test_cases.each do |tc|
       java_code = 'public class Code{
@@ -29,31 +27,34 @@ class Java < Code
           public static void main(String args[]){
 
             Code c = new Code();
-            int x = c.' + tc.input + ';
+            ' + tc.return_type + ' x = c.' + tc.input + ';
             System.out.println(""+x);
 
           }
         }'
 
-      my_file = File.new("Code.java", "w+")
-      my_file.puts(java_code)
-      my_file.close
-      File.chmod(0755,"Code.java")
-      stdin, stdout, stderr = Open3.popen3('javac Code.java')
+      Dir.chdir(dir_path) do
 
-      puts stderr.gets
+        my_file = File.new("Code.java", "w+")
+        my_file.puts(java_code)
+        my_file.close
+        File.chmod(0755,"Code.java")
+        stdin, stdout, stderr = Open3.popen3('javac Code.java')
+        puts stderr.gets
 
-      if File.exist?("Code.class")
+        if File.exist?("Code.class")
 
-        $result = `timeout 4s java Code`
-        File.delete("Code.java") if File.exist?("Code.java")
-        File.delete("Code.class") if File.exist?("Code.class")
+          $result = `timeout 4s java Code`
+          File.delete("Code.java") if File.exist?("Code.java")
+          File.delete("Code.class") if File.exist?("Code.class")
 
-        return false  if $result.chomp != tc.output
+          return false  if $result.chomp != tc.output
 
-      else
+        else
 
-        return false
+          return false
+
+        end
 
       end
 
